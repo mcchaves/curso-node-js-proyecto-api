b06bcc84cdd8e1bf5368aa4d6d315ae524dff7de
mcchaves <eleceya.2@gmail.com>
2018-10-03 16:49:44 +0200

    agregados post y delete

--- app/server.js
+++ app/server.js
@@ -4,13 +4,25 @@ var app = express()
 var port = process.env.PORT || 8080 // establecemos nuestro puerto
 
 app.get('/', (req, res) => {
-  res.json({ mensaje: '¡Hola Mundo!' })
+  res.json({ mensaje: '¡Hola Gente!' })
 })
 
 app.get('/cervezas', (req, res) => {
   res.json({ mensaje: '¡A beber cerveza!' })
 })
 
+app.post('/cervezas', (req, res) => {
+  res.json({ mensaje: 'Cerveza guardada' })
+})
+
+app.delete('/cervezas', (req, res) => {
+  res.json({ mensaje: 'Cerveza borrada' })
+})
+
 // iniciamos nuestro servidor
-app.listen(port)
-console.log('API escuchando en el puerto ' + port)
+// app.listen(port)
+app.listen(port, () => {
+  console.log(`API escuchando por el puerto ${port}`)
+})
+
+console.log('API todavía no está escuchando')
