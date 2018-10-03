45e00cc459e462fd987e36c1fee57d195b52b57d
mcchaves <eleceya.2@gmail.com>
2018-10-03 18:36:03 +0200

    Modificados los router para búsqueda por paralabra clave, por id, y actualización

--- app/routes/cervezas.js
+++ app/routes/cervezas.js
@@ -1,16 +1,29 @@
 const express = require('express')
 const router = express.Router()
 
+// PRIMERO PONDREMOS LA RUTA DE SEARCH
+router.get('/search', (req, res) => {
+  res.json({ mensaje: 'Muestro las cervezas por keyword' })
+})
+
 router.get('/', (req, res) => {
-  res.json({ mensaje: '¡A beber cerveza!' })
+  res.json({ mensaje: 'Aquí tienes todas las cervezas. ¡A beber cerveza!' })
+})
+
+router.get('/:id', (req, res) => {
+  res.json({ mensaje: `Esta es la cerveza con ${req.params.id}` })
 })
 
 router.post('/', (req, res) => {
   res.json({ mensaje: 'Cerveza guardada' })
 })
 
-router.delete('/', (req, res) => {
-  res.json({ mensaje: 'Cerveza borrada' })
+router.put('/:id', (req, res) => {
+  res.json({ mensaje: `Cerveza con ${req.params.id} actualizada` })
+})
+
+router.delete('/:id', (req, res) => {
+  res.json({ mensaje: `Cerveza con ${req.params.id} borrada` })
 })
 
 module.exports = router
