8c6c2aebfa7512f8be85653343b2b18ca44fca5c
mcchaves <eleceya.2@gmail.com>
2018-10-04 19:59:48 +0200

    añadido codigo en cervezasController

--- app/controllers/cervezasController.js
+++ app/controllers/cervezasController.js
@@ -1,3 +1,4 @@
+const Cervezas = require('../models/Cervezas')
 
 const search = (req, res) => {
   const q = req.query.q
@@ -5,26 +6,97 @@ const search = (req, res) => {
 }
 
 const list = (req, res) => {
-  res.send({ mensaje: 'Lista de todas las cervezas' })
+  // listo todas las cervezas
+  Cervezas.find((err, cervezas) => {
+    if (err) {
+      res.status(500).send()
+    }
+    res.status(200).send(cervezas)
+  })
 }
 
+// busco una cerveza por id
 const show = (req, res) => {
   const id = req.params.id
-  res.send({ mensaje: `Cerveza ${id} buscada` })
+  Cervezas.findById({ _id: id }, (err, cerveza) => {
+    if (err) {
+      res.send('Error: error')
+    }
+    if (cerveza === null) return res.status(404).send({})
+    res.send(cerveza)
+  })
 }
 
+// Creo nueva cerveza
 const create = (req, res) => {
-  res.send({ mensaje: `Guardada una cerveza` })
+  const cerveza = new Cervezas(req.body)
+  cerveza.save((err, cerveza) => {
+    if (err) {
+      return res.status(400).json({
+        message: 'Error al guardar la cerveza',
+        error: err
+      })
+    }
+    return res.status(201).json(cerveza)
+  })
+}
+module.exports = {
+  search,
+  list,
+  show,
+  create
 }
 
 const update = (req, res) => {
   const id = req.params.id
-  res.send({ mensaje: `Actualizada la cerveza con id ${id}` })
+  Cervezas.findOne({ _id: id }, (err, cerveza) => {
+    if (!Cervezas.ObjectId.isValid(id)) {
+      return res.status(404).send()
+    }
+    if (err) {
+      return res.status(500).json({
+        message: 'Se ha producido un error al guardar la cerveza',
+        error: err
+      })
+    }
+    if (!ObjectId.isValid(id)) {
+      return res.status(404).send()
+    }
+    if (!cerveza) {
+      return res.status(404).json({
+        message: 'No hemos encontrado la cerveza'
+      })
+    }
+
+    Object.assign(cerveza, req.body)
+
+    cerveza.save((err, cerveza) => {
+      if (err) {
+        return res.status(500).json({
+          message: 'Error al guardar la cerveza'
+        })
+      }
+      if (!cerveza) {
+        return res.status(404).json({
+          message: 'No hemos encontrado la cerveza'
+        })
+      }
+      return res.json(cerveza)
+    })
+  })
+}
+module.exports = {
+  search,
+  list,
+  show,
+  create,
+  update
 }
 
 const remove = (req, res) => {
   const id = req.params.id
-  res.send({ mensaje: `Borrada la cerveza con id ${id}` })
+  // res.send({ mensaje: `Borrada la cerveza con id ${id}` })
+  Cervezas.findByIdAndRemove({ _id: id })
 }
 
 const cervezasController = {
--- app/server.js
+++ app/server.js
@@ -17,3 +17,5 @@ app.listen(port, () => {
 })
 
 console.log('API todavía no está escuchando')
+
+module.exports = app
