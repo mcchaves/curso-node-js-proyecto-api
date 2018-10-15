3faf26fff18b8ba5a504a1f935921fbc4da34bf0
mcchaves <eleceya.2@gmail.com>
2018-10-15 19:21:20 +0200

    añadido morgan y cors

--- app/server.js
+++ app/server.js
@@ -2,11 +2,16 @@ const express = require('express') // llamamos a Express
 const app = express()
 const router = require('./routes')
 const bodyParser = require('body-parser')
+const cors = require('cors')
+const morgan = require('morgan')
+
 require('./db')
 
 // configuración de middlewares
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
+app.use(morgan('combined'))
+app.use(cors())
 
 // nuestra ruta irá en http://localhost:8080/api
 var port = process.env.PORT || 8080 // establecemos nuestro puerto
